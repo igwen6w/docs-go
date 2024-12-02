a58c7857fb06228dadb16b9ea4bed4db8f61e0e2
igwen6w <igwen6w@gmail.com>
2024-12-03 00:22:47 +0800

    welcome.rpc

---
+++ welcomerpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "docs/welcomerpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
